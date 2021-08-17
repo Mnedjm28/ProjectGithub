5bc35033c9813688a9416a809d9103ee1beec1ae
Mnedjmo28 <m.nedjmo726@gmail.com>
2021-08-17 15:04:33 +0100

    Create Classes in Layers.Entities

--- Projectgithub/Projectgithub.Data.Layers/Entities/Departement.cs
+++ Projectgithub/Projectgithub.Data.Layers/Entities/Departement.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projectgithub.Data.Layers.Entities
 {
-    class Departement
+    public class Departement
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public virtual ICollection<Client> Clients { get; set; }
     }
 }
