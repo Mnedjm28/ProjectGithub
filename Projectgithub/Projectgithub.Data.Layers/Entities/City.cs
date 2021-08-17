5401fe64a1740ccb37665523c1d2977d7c427688
Mnedjmo28 <m.nedjmo726@gmail.com>
2021-08-17 15:02:00 +0100

    Create Classes in Layers.Entities

---
+++ Projectgithub/Projectgithub.Data.Layers/Entities/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectgithub.Data.Layers.Entities
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public virtual ICollection<Client> Clients { get; set; }
+    }
+}
---
+++ Projectgithub/Projectgithub.Data.Layers/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectgithub.Data.Layers.Entities
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Email { get; set; }
+
+        public String PhoneNumber { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public Departement Departement { get; set; }
+
+        public City City { get; set; }
+    }
+}
